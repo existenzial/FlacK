41958ae087bfd21a2ce9de8a9b8b4b76c32b8ea1
Jazz Lyles <ripley36706@gmail.com>
2016-01-24 00:40:46 -0800

    channels clickable, working on retaining sessions

---
+++ client/channel.js
@@ -0,0 +1,18 @@
+Meteor.startup(function(){
+    Session.set('channel', 'general');
+});
+
+Template.channel.events({
+    'click .channel': function(e){
+        Session.set('channel', this.name);
+    }
+});
+
+Template.channel.helpers({
+    active: function(){
+        if (Session.get('channel') === this.name){
+            return 'active';
+        }
+        return '';
+    }
+});
